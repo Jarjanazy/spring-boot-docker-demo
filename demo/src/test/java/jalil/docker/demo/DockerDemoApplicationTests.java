6239a7acc9746aa31019c0fd6fb4a10dd8cb3ebd
cercenazi <celil.cercenazi@gmail.com>
2021-08-21 10:48:10 +0300

    add the initial project

---
+++ demo/src/test/java/jalil/docker/demo/DockerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package jalil.docker.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
